ff1cae8f87e2bcd833281ff5ad30edc16eb30a96
Densodim <25denso@gmail.com>
2026-04-13 16:28:41 +0300

    use jvm

--- composeApp/src/jvmMain/kotlin/com/example/appshoptour/main.kt
+++ composeApp/src/jvmMain/kotlin/com/example/appshoptour/main.kt
@@ -2,12 +2,22 @@ package com.example.appshoptour
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.example.appshoptour.di.sharedModules
+import org.koin.core.context.startKoin
 
-fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "AppShopTour",
-    ) {
-        App()
+private const val DESKTOP_BASE_URL = "http://91.84.122.246:8080/api/v1"
+
+fun main() {
+    startKoin {
+        modules(sharedModules(DESKTOP_BASE_URL))
+    }
+
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "AppShopTour",
+        ) {
+            App()
+        }
     }
 }
